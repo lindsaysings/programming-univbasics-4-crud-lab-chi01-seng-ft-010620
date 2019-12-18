4ea7161831f73378c1c16300408e00afd6d5f209
Lindsay M <lindsaymecher@gmail.com>
2019-12-18 00:30:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
+
 def create_an_empty_array
   []
 end
 
 def create_an_array
-  ["ham", "cheese", "bread", "mustard"]
+  [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  array << (element)
+def add_element_to_end_of_array(array,element)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -35,6 +36,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-# element = array[index_number] too much. Just asks for updated element.
-element 
+  array[index_number] = element
 end
+
